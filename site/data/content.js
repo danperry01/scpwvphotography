de4e86dd851921e883a0d21b9fba1700aa1c48dc
Dan Perry <83229842+danperry01@users.noreply.github.com>
2026-03-08 16:39:53 -0400

    Add gallery photo: img-3516-2-1773002385319.jpg

--- site/data/content.js
+++ site/data/content.js
@@ -113,6 +113,12 @@ window.SCP = /* DATA_START */
         "alt": "img-5323-1773002374286.jpg",
         "category": "nature",
         "featured": false
+      },
+      {
+        "src": "assets/images/gallery/nature/img-3516-2-1773002385319.jpg",
+        "alt": "img-3516-2-1773002385319.jpg",
+        "category": "nature",
+        "featured": false
       }
     ]
   },
