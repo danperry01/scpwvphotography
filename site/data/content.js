ff17547460c424a6ac5855e082e59bf6e1dfb732
Dan Perry <83229842+danperry01@users.noreply.github.com>
2026-03-08 16:44:07 -0400

    Update home page highlights

--- site/data/content.js
+++ site/data/content.js
@@ -137,7 +137,8 @@ window.SCP = /* DATA_START */
     ],
     "homePage": [
       "assets/images/gallery/sports/sports-01.jpg",
-      "assets/images/gallery/sports/img-4294-1772999464374.jpg"
+      "assets/images/gallery/sports/img-4294-1772999464374.jpg",
+      "assets/images/gallery/sports/img-4304-1772999470257.jpg"
     ]
   },
   "services": [
