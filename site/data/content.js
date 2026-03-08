937d468192cbc26a961d9944c9bb1227aa7954ae
Dan Perry <83229842+danperry01@users.noreply.github.com>
2026-03-08 16:39:36 -0400

    Add gallery photo: img-5323-1773002374286.jpg

--- site/data/content.js
+++ site/data/content.js
@@ -107,6 +107,12 @@ window.SCP = /* DATA_START */
         "alt": "img-4556-1773002368402.jpg",
         "category": "nature",
         "featured": false
+      },
+      {
+        "src": "assets/images/gallery/nature/img-5323-1773002374286.jpg",
+        "alt": "img-5323-1773002374286.jpg",
+        "category": "nature",
+        "featured": false
       }
     ]
   },
