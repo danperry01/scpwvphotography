23acd12ef8ec10c0735f52fccfe9e9b2c337cd51
Dan Perry <83229842+danperry01@users.noreply.github.com>
2026-03-09 07:57:42 -0400

    Remove gallery photo at index 16

--- site/data/content.js
+++ site/data/content.js
@@ -121,12 +121,6 @@ window.SCP = /* DATA_START */
         "featured": false
       },
       {
-        "src": "assets/images/gallery/nature/img-4648-1773002401437.jpg",
-        "alt": "img-4648-1773002401437.jpg",
-        "category": "nature",
-        "featured": false
-      },
-      {
         "src": "assets/images/gallery/nature/img-5323-1773002840535.jpg",
         "alt": "img-5323-1773002840535.jpg",
         "category": "nature",
